33d985f22992bbffa547edc6817369149c999275
nerd8622 <nerd8622@gmail.com>
2021-07-28 16:12:30 -0600

    used consistent variable names

--- server/server.js
+++ server/server.js
@@ -26,17 +26,17 @@ app.post('/auth', (req, res) => {
   if (username && password) {
 		sqlConnection.query('SELECT * FROM accounts WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
 			if (results.length > 0) {
-				request.session.loggedin = true;
-				request.session.username = username;
-				response.redirect('/');
+				req.session.loggedin = true;
+				req.session.username = username;
+				res.redirect('/');
 			} else {
-				response.send('Incorrect Username and/or Password!');
+				res.send('Incorrect Username and/or Password!');
 			}
-			response.end();
+			res.end();
 		});
 	} else {
-		response.send('Please enter Username and Password!');
-		response.end();
+		res.send('Please enter Username and Password!');
+		res.end();
 	}
 });
 
