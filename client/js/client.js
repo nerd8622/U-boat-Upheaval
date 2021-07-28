215d3ffb3757bd69fe23d31c8d4dc7fc8894d735
nerd8622 <nerd8622@gmail.com>
2021-07-28 11:10:02 -0600

    added chat feature

--- client/js/client.js
+++ client/js/client.js
@@ -1,4 +1,34 @@
+var logged_in = false;
+
+const displayChat = ([name, color, message]) => {
+  const listEl = document.querySelector('#messages');
+  const newEl = document.createElement('li');
+  const nameSp = document.createElement('span');
+  nameSp.style.color = color;
+  nameSp.innerHTML = name + ": ";
+  newEl.appendChild(nameSp);
+  newEl.innerHTML += message;
+  listEl.appendChild(newEl);
+  listEl.scrollTop = listEl.scrollHeight;
+};
+
+const sendChat = (sock) => (e) => {
+  e.preventDefault();
+  const input = document.querySelector('#message-box');
+  const text = input.value;
+  input.value = "";
+  if (!logged_in){
+    sock.emit('login', text);
+    logged_in = true;
+  }
+  else {
+    sock.emit('chat-message', text);
+  }
+};
+
 (() => {
   const sock = io();
-  sock.on('message', console.log);
+  sock.on('chat-message', displayChat);
+
+  document.querySelector('#chat-form').addEventListener('submit', sendChat(sock));
 })();
--- server/server.js
+++ server/server.js
@@ -1,10 +1,11 @@
 const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
-
+const randomColor = require('randomcolor');
+const sanitizeHtml = require('sanitize-html');
 const app = express();
 
-app.use(express.static(`{__dirname}/../client`));
+app.use(express.static(`${__dirname}/../client`));
 
 const server = http.createServer(app);
 const port = 8080;
@@ -12,6 +13,23 @@ const io = socketio(server);
 
 io.on('connection', (sock) => {
   console.log('Connection recieved!');
+  const serverMsg = (msg) => {
+    return ['Server', '#111111', msg]
+  };
+  sock.emit('chat-message', serverMsg('Welcome to U-boat Upheaval, please enter a name!'));
+  sock.on('login', (message) => {
+    const color = randomColor();
+    const name = message;
+    sock.emit('chat-message', serverMsg('Hello ' + name + '! You can now use chat!'))
+    const addName = (msg) => {
+      let safe = sanitizeHtml(msg, {allowedTags: [ 'b', 'i' ], allowedAttributes: {}});
+      return [name, color, safe];
+    };
+
+    sock.on('chat-message', (message) => {
+      io.emit('chat-message', addName(message));
+    });
+  });
 });
 
 server.on('error', (error) => {
@@ -19,5 +37,5 @@ server.on('error', (error) => {
 });
 
 server.listen(port, () => {
-  console.log('Http server running on port' + port);
+  console.log('Http server running on port ' + port);
 });
