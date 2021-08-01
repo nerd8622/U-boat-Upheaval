0e321a5caf72dde77f2e486b5667dfcc8c3cb25b
nerd8622 <nerd8622@gmail.com>
2021-08-01 00:30:25 -0600

    efficiency

--- server/server.js
+++ server/server.js
@@ -32,12 +32,13 @@ const port = 8123;
 const server = http.createServer(app);
 const io = socketio(server);
 io.use((socket, next) => {sessionMiddleware(socket.request, {}, next);});
+let hash256 = crypto.createHash("sha256");
 const { getBoard, addPlayer } = game(22, 12);
 let players = new Map();
 
 app.post('/auth', (req, res) => {
   let username = req.body.usr;
-  let password = crypto.createHash("sha256").update(req.body.psw).digest("base64");
+  let password = hash256.update(req.body.psw).digest("base64");
   if (username && password) {
 		sqlConnection.query('SELECT * FROM accounts WHERE username = ? AND password = ?', [username, password], (error, results, fields) => {
 			if (results.length > 0) {
@@ -55,7 +56,7 @@ app.post('/auth', (req, res) => {
 
 app.post('/register', (req, res) => {
   let username = req.body.usr;
-  let password = crypto.createHash("sha256").update(req.body.psw).digest("base64");
+  let password = hash256.update(req.body.psw).digest("base64");
   let email = req.body.email;
   if (username && password && email) {
     sqlConnection.query('SELECT * FROM accounts WHERE username = ? and email = ?', [username, email], (error, results, fields) => {
