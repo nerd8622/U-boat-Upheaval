5da361bf2f8d81b87e8940001d1fef246af2e434
nerd8622 <nerd8622@gmail.com>
2022-01-08 10:19:24 -0700

    ok

--- client/static/js/client.js
+++ client/static/js/client.js
@@ -159,7 +159,7 @@ const makeGame = (canvas, xCells, yCells) => {
     //ctx.fillStyle = '#232323';
     //if (isMe) {ctx.fillStyle = '#5c5c5c'};
     //ctx.beginPath();
-    submarine_img.draw(x*ySize + ySize/2, y*xSize + xSize/2);
+    submarine_img.draw(x*ySize, y*xSize);
     //ctx.ellipse(x*ySize + ySize/2, y*xSize + xSize/2, sleng, swidt, 0, 0, 2 * Math.PI);
     //ctx.fill();
   };
